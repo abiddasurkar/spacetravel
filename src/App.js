8ad617b39a9d18b47ede63871f5d28f884139a12
abiddasurkar <abiddasurkar@gmail.com>
2025-03-14 19:38:20 +0530

    removved unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import React, { useState } from 'react';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import LandingPage from './components/LandingPage';
-// import BrowseTrips from './components/BrowseTrips';
-// import TripDetails from './components/TripDetails';
-// import Dashboard from './components/Dashboard';
-// import Accommodations from './components/Accommodations';
-
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import LandingPage from './components/LandingPage/LandingPage';
